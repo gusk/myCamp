3395f0c5e5c24634914400a91245810a03e42891
Gus King <gusssk@gmail.com>
2014-07-14 14:01:27 -0600

    checking for mobile device

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -68,4 +68,17 @@ function deleteMarkers() {
   markers = [];
 }
 
+
+function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map-canvas");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '95%';
+  }
+}
 //google.maps.event.addDomListener(window, 'load', initialize);
